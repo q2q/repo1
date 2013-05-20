5183787366a21183b2c88c94c7508075a4543256
rservice2 <evankingsford@rogiservice.com>
2013-05-19 20:29:58 -0700

    modified iconnect script

--- iconnect.sh
+++ iconnect.sh
@@ -12,8 +12,8 @@ fi
 
 SCPPATH="~/IDCF/s2.sh"
 #FILE=$( echo $SCPPATH | cut -d '/'
-MINEPWD='x'
-HOSTNAME="at"
+MINEPWD='pass'
+HOSTNAME="amz"
 
 Secure() {
 	ssh -o StrictHostkeyChecking=no $HOSTNAME$i " echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config "
@@ -24,23 +24,27 @@ Transfer() {
         ssh -o StrictHostkeyChecking=no $HOSTNAME$i "screen -d -m sh s2.sh"
         }
         
-T-amazon() {
+Tamazon() {
 	GITURL="https://github.com/q2q/repo1.git"
 	REPO="repo1"
-	ssh -o StrictHostkeyChecking=no ${HOSTNAME}$i \ 
-	"sudo apt-get update; sudo apt-get -y install git; git clone $GITURL; sh $REPO/initscript.sh"
+	ssh -o StrictHostkeyChecking=no ${HOSTNAME}${i} "sudo apt-get update ; sudo apt-get -y install git ; git clone $GITURL ; sh $REPO/initscript.sh"
 	}	
 
 Mine () {
-	REPO="repo1
-    ssh -o StrictHostkeyChecking=no ${HOSTNAME}$i \	
-    	"cd cpuminer; screen -d -m ./minerd -a scrypt-jane --url=http://mineyac2.dontmine.me:8080 --userpass=rogiservice.${SWN}:pass
-    
-    
+	REPO="repo1"
+    ssh -o StrictHostkeyChecking=no ${HOSTNAME}$i \
+"cd cpuminer; screen -d -m ./minerd -a scrypt-jane --url=http://mineyac2.dontmine.me:8080 --userpass=rogiservice.${SWN}:pass"
+#"cd cpuminer; screen -d -m ./minerd -a scrypt-jane --url=168.61.9.67:9323 --userpass=user:pass"     
     #ssh $HOSTNAME$i "screen -d -m ./gbt2 -a scrypt-jane --url=mineyac2.dontmine.me:8080 --userpass=rogiservice.$s:$MINEPWD"
 	#ssh idcf$i "screen -d -m ./gbt --url=us.litecoinpool.org:9332 --userpass=wetroof.$s:4444"
-	SWN=$[$SWN+$i]
+	SWN=$[$SWN+1]
+	echo $SWN
         }
+
+Kill () {
+	 ssh -o StrictHostkeyChecking=no ${HOSTNAME}$i \
+"pkill minerd"
+	}
 
 Reboot () {
         ssh $HOSTNAME$i "reboot"
